382b27aaff57f6cf5519fc8066d057060e6b6f01
唐都 <7277710@qq.com>
2018-12-11 10:18:22 +0800

    1.添加头文件 2.0.5.1

---
+++ Pod/Classes/GTCommonKit.h
@@ -0,0 +1,31 @@
+//
+//  GTCommonKit.h
+//  TestPods
+//
+//  Created by mac on 2018/12/11.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#ifndef GTCommonKit_h
+#define GTCommonKit_h
+
+/***********************************Category*************************************/
+#import "NSArray+GTAarray.h"
+#import "NSDictionary+Category.h"
+#import "NSDictionary+Safe.h"
+#import "NSMutableDictionary+Safe.h"
+#import "NSString+Category.h"
+#import "NSString+DisplayTime.h"
+#import "NSString+MD5.h"
+#import "NSString+Predicate.h"
+#import "NSString+Safe.h"
+#import "UIScrollView+GTScrollView.h"
+#import "UITableView+GTTableView.h"
+
+
+
+
+
+
+
+#endif /* GTCommonKit_h */
